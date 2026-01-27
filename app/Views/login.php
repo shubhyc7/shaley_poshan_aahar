c3f05e7a76affdc1f3694f946065eeb56ec3ef20
Shubham <shubham.chaudhary@mattersoft.xyz>
2026-01-27 12:33:50 +0530

    favicon icon added

--- app/Views/login.php
+++ app/Views/login.php
@@ -3,6 +3,10 @@
 
 <head>
     <title>Login - Poshan Aahar</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= base_url('favicon_io/apple-touch-icon.png') ?>">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= base_url('favicon_io/apple-touch-icon.png') ?>">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= base_url('favicon_io/favicon-32x32.png') ?>">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= base_url('favicon_io/favicon-16x16.png') ?>">
     <link href="<?= base_url('css/bootstrap.min.css') ?>" rel="stylesheet">
     <style>
         body {
